2e34a0354a5c0734ec80d856496d0c823fa12490
Gabriela <54961860+gabrielaspooky@users.noreply.github.com>
2024-04-28 14:32:22 +0200

    new file

---
+++ datosyvariables.js
@@ -0,0 +1,17 @@
+// 1.- Una tortilla de patatas lleva 200 gramos de patatas por persona. Por cada kilo de patatas se necesitan 5 huevos y 300 gramos de cebolla. Escribe un código que, dado el número de comensales, calcule las cantidades de ingredientes necesarios y muestre por consola, un mensaje que indique el numero de comensales, y la cantidad de cada uno de los ingredientes.
+// Ej: 5 comensales,
+// Se necesitará:
+// 1 kg de papas
+// 5 huevos
+// 300 gr de cebolla.
+
+// 2.- Dado un número por el usuario, devuelve por consola "true" si es un número par y "false" si es un número impar. No usar condicionales!!! aunque, puedes usar operadores de comparación.
+// Ej.: numero: 50
+// ¿Es par? : true
+
+// 3.- En este ejercicio debes convertir a segundos una medida de tiempo dada en horas y minutos. ¿Crees que es necesario recordar que una hora son 60 minutos y cada minuto son 60 segundos? ¿Por dónde mostraras el resultado?
+// Ej.: horas : 2
+// minutos 30
+// Resultado 2*60*60 + 30*60 = 9000 segundos.
+
+// 4.-  Muestra en un aviso (alert), cuánto debe pagar un usuario por un producto con IVA(21%). ¿Tienes alguna manera de preguntarle el precio del producto al usuario?
